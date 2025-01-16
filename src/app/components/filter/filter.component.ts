a15851d7249b6751bbbf29e233ad00565e4af750
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-01-16 16:18:10 -0300

    feat : add FilterComponent

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { Status } from '../../enum/ststus';
+import { FilterTaskService } from '../../services/filter/filter-task.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+
+  ],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.scss'
+})
+export class FilterComponent {
+  protected filterTaskService = inject(FilterTaskService)
+
+  public select = new FormControl<Status | null>(null);
+
+  constructor() {
+    this.select.valueChanges.subscribe(ststus => {
+      this.onEventFilter(ststus!);
+    })
+  }
+
+  protected status = Status;
+  public onEventFilter(status: Status) {
+    this.filterTaskService.status.set(status)
+    this.filterTaskService.eventEmmiterFromFilter()
+  }
+
+  public onEventReset = () => {
+    this.filterTaskService.eventEmmiterReset();
+    this.select.reset();
+  }
+}
