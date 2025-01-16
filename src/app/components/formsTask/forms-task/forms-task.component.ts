0d1ce91f859cfaeaa33315cb6453096a5c933f64
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-01-16 16:21:25 -0300

    refactor : FormsTaskComponent

--- src/app/components/formsTask/forms-task/forms-task.component.ts
+++ src/app/components/formsTask/forms-task/forms-task.component.ts
@@ -1,6 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ITask } from '../../../interfaces/task';
+import { DatabaseService } from '../../../services/database/database.service';
+import { v4 as uuidv4 } from 'uuid';
+import { Status } from '../../../enum/ststus';
+import { FormsTaskService } from '../../../services/formsTask/forms-task.service';
+import { Task } from 'zone.js/lib/zone-impl';
+import { Router } from '@angular/router';
 
+
+export interface IFrom {
+  conteudo: string,
+  titulo: Status
+}
 @Component({
   selector: 'app-forms-task',
   standalone: true,
@@ -12,10 +24,45 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class FormsTaskComponent {
 
-  protected from = new FormGroup({
-    titulo: new FormControl<string>(""),
-    conteudo: new FormControl<string>("")
+  protected data = inject(DatabaseService);
+  protected formsTaskService = inject(FormsTaskService);
+  protected task: ITask | null = this.formsTaskService.getTaskFromForm();
+  protected router = inject(Router);
+
+  protected form = new FormGroup({
+    titulo: new FormControl<string>(
+      this.task?.titulo ?? ""
+    ),
+    conteudo: new FormControl<string>(
+      this.task?.conteudo ?? ""
+    )
+
   });
 
-  
+
+  public onEventTask() {
+
+    const task = (this.form.value as ITask);
+    task.id = uuidv4();
+    task.status = Status.aFazer;
+    this.data.setOnTask(task);
+    this.form.reset();
+  }
+  public onEventUpdateContent() {
+
+    const updateTask: ITask =
+    {
+      id: this.task!.id,
+      status: this.task!.status,
+      ...this.form.value as IFrom,
+    }
+
+    this.data.udapteTaskContent(updateTask)
+    this.form.reset();
+    this.formsTaskService.resetTaskFromForm();
+    this.router.navigate(['list'])
+
+
+  }
+
 }
