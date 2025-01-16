6f8c14775542b53f326660621519cb8f8bb9b227
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-01-16 16:19:58 -0300

    feat : add ModalComponent

---
+++ src/app/components/modal/modal/modal.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject, inject } from '@angular/core';
+import { ModalService } from '../../../services/modal/modal.service';
+import { ITask } from '../../../interfaces/task';
+import { Status } from '../../../enum/ststus';
+import { DatabaseService } from '../../../services/database/database.service';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+  protected data = inject(DatabaseService)
+
+  public modalService = inject(ModalService)
+  public open = () => this.modalService.getOpen();
+   public task = () => this.modalService.getTask();
+  public ststus = Status;
+
+  public renove(id: string) {
+    this.data.removerTask(id);
+    this.modalService.close();
+  }
+  public updateStatus(task: ITask, Status: Status) {
+    this.data.udapteStatus(task, Status);
+  }
+
+
+
+
+
+}
